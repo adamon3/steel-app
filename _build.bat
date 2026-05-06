11f555281d825109af0b9062d3c9ae9392f8dde0
Adamon3 <adamon3@yahoo.com>
2026-05-06 06:17:16 +0100

    Clean up: remove temp files from repo, update .gitignore

--- _build.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-cd /d "C:\Users\adamo\OneDrive\Desktop\steel-app"
-echo === GIT STATUS ===
-"C:\Program Files\Git\cmd\git.exe" status
-echo === GIT REMOTE ===
-"C:\Program Files\Git\cmd\git.exe" remote -v
-echo === GIT LOG ===
-"C:\Program Files\Git\cmd\git.exe" log --oneline -5
-echo === DONE ===
